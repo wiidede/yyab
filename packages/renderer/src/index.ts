bc2a06e6ef532e2c923d4012bd1bb27ba4f427f6
wiidede <wiixdede@gmail.com>
2021-08-17 17:28:51 +0800

    [框架] 引入Element Plus

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,7 +1,42 @@
 import {createApp} from 'vue';
 import App from '/@/App.vue';
 import router from '/@/router';
+// 如果要使用.scss样式文件，则需要引入base.scss文件
+import 'element-plus/packages/theme-chalk/src/base.scss';
 
-createApp(App)
+import {
+  ElButton,
+  ElSelect,
+  ElScrollbar,
+  ElLoading,
+  ElMessage,
+  ElMessageBox,
+  ElNotification,
+} from 'element-plus';
+
+const components = [
+  ElButton,
+  ElSelect,
+  ElScrollbar,
+];
+
+const plugins = [
+  ElLoading,
+  ElMessage,
+  ElMessageBox,
+  ElNotification,
+];
+
+const app = createApp(App);
+
+components.forEach(component => {
+  app.component(component.name, component);
+});
+
+plugins.forEach(plugin => {
+  app.use(plugin);
+});
+
+app
   .use(router)
   .mount('#app');
--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -3,23 +3,38 @@
 import {chrome} from '../../electron-vendors.config.json';
 import {join} from 'path';
 import {builtinModules} from 'module';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import styleImport from 'vite-plugin-style-import';
 
 const PACKAGE_ROOT = __dirname;
 
-/**
- * @type {import('vite').UserConfig}
- * @see https://vitejs.dev/config/
- */
-const config = {
+export default defineConfig({
   mode: process.env.MODE,
   root: PACKAGE_ROOT,
   resolve: {
     alias: {
       '/@/': join(PACKAGE_ROOT, 'src') + '/',
+      '/@assets/': join(PACKAGE_ROOT, 'assets') + '/',
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [{
+        libraryName: 'element-plus',
+        esModule: true,
+        ensureStyleFile: true,
+        resolveStyle: (name) => {
+          name = name.slice(3);
+          return `element-plus/packages/theme-chalk/src/${name}.scss`;
+        },
+        resolveComponent: (name) => {
+          return `element-plus/lib/${name}`;
+        },
+      }],
+    }),
+  ],
   base: '',
   server: {
     fs: {
@@ -46,6 +61,4 @@ const config = {
     emptyOutDir: true,
     brotliSize: false,
   },
-};
-
-export default config;
+});
